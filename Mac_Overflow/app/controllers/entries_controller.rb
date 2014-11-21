c15439cbabf4a9c76bab8a6c3849eae3d603c4a8
Michael Angelo <yamikamisama@gmail.com>
2014-11-20 20:54:06 -0500

    Added Create Entry into Entry Controller

--- Mac_Overflow/app/controllers/entries_controller.rb
+++ Mac_Overflow/app/controllers/entries_controller.rb
@@ -1,11 +1,23 @@
 class EntriesController < ApplicationController
 
 	def index
+		@entry = Entry.new
 		@entries = Entry.all
 	end
 
 	def show
 		@entry = Entry.find(params[:id])
+	end
+
+	def create
+		Entry.create(entry_params)
+		redirect_to entries_path
+	end
+
+	private
+	
+	def entry_params
+		params.require(:entry).permit(:title, :content)
 	end
 
 end
