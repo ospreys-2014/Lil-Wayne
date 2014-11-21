aa00fb57731d30592c060503b4e3fabc1079f60b
Corey Nilan <cnilan@gmail.com>
2014-11-20 23:21:31 -0500

    Sign-up functionality complete.

--- Mac_Overflow/app/models/user.rb
+++ Mac_Overflow/app/models/user.rb
@@ -1,4 +1,8 @@
 class User < ActiveRecord::Base
    has_secure_password
    has_many :entries
+
+   validates :name, presence: true, length: { maximum: 50 }
+   validates :password, length: { minimum: 3 }
+
 end
