3565453723671d98542c61483543904096523b97
Corey Nilan <cnilan@gmail.com>
2014-11-21 11:33:44 -0500

    added minimum length to username

--- Mac_Overflow/app/models/user.rb
+++ Mac_Overflow/app/models/user.rb
@@ -2,7 +2,7 @@ class User < ActiveRecord::Base
    has_secure_password
    has_many :entries
 
-   validates :name, presence: true, length: { maximum: 50 }
+   validates :name, presence: true, length: { minimum: 2, maximum: 50 }
    validates :password, length: { minimum: 3 }
 
 end
