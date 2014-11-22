93c77e042ac9559090fbd57701eb6bfb3834af24
Michael Angelo <yamikamisama@gmail.com>
2014-11-22 13:09:03 -0500

    Added Comment model (can become recursive), fixed all code to accept new model and function correctly

--- Mac_Overflow/app/models/entry.rb
+++ Mac_Overflow/app/models/entry.rb
@@ -1,6 +1,5 @@
 class Entry < ActiveRecord::Base
-  belongs_to :parent, class_name: "Entry", foreign_key: :parent_id
-  has_many :children, class_name: "Entry", foreign_key: :parent_id
+ 	has_many :comments
 
   belongs_to :user
 end
--- Mac_Overflow/app/models/user.rb
+++ Mac_Overflow/app/models/user.rb
@@ -1,6 +1,7 @@
 class User < ActiveRecord::Base
    has_secure_password
    has_many :entries
+   has_many :comments
 
    validates :name, presence: true, length: { minimum: 2, maximum: 50 }
    validates :password, length: { minimum: 3 }
